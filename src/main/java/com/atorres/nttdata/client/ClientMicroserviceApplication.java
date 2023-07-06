27557488681a8407bffad77dcecc2cc26a9ff6fc
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-05 20:55:46 -0500

    commit initial

---
+++ src/main/java/com/atorres/nttdata/client/ClientMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientMicroserviceApplication.class, args);
+	}
+
+}
