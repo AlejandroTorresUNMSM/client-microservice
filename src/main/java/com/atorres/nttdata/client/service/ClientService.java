1c893eccb26a85f5be4d5baace154c297db552c6
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-08 12:56:59 -0500

    Capturando la excepcion y devolviendo un mensaje de error

--- src/main/java/com/atorres/nttdata/client/service/ClientService.java
+++ src/main/java/com/atorres/nttdata/client/service/ClientService.java
@@ -1,10 +1,12 @@
 package com.atorres.nttdata.client.service;
 
+import com.atorres.nttdata.client.exception.CustomException;
 import com.atorres.nttdata.client.mapper.ClientMapper;
 import com.atorres.nttdata.client.model.ClientPost;
 import com.atorres.nttdata.client.model.dao.ClientDao;
 import com.atorres.nttdata.client.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -27,8 +29,7 @@ public class ClientService {
     public Mono<ClientDao> save(ClientPost client) {
         Mono<Boolean> existeClient = dao.findByNroDocument(client.getNroDocument()).hasElement();
 
-        //return dao.findByNroDocument(client.getNroDocument()).switchIfEmpty(dao.insert(clientMapper.clientRequesttoClientDao(client)));
-        return existeClient.flatMap(exist -> exist ? Mono.error(new Exception("El client existe"))
+        return existeClient.flatMap(exist -> exist ? Mono.error(new CustomException(HttpStatus.BAD_REQUEST,"El cliente Existe"))
                 : dao.save(clientMapper.clientRequesttoClientDao(client)));
     }
 
